fd1f173a7686de72207e6d84f086d0506e889241
Mark Janssen <mark@praseodym.net>
2022-12-03 11:04:50 +0100

    Day 3: part two

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -8,28 +8,37 @@ fn main() {
 
 fn run(input: &'static str) -> (u32, u32) {
     let mut part1_answer: u32 = 0;
-    let part2_answer: u32 = 0;
+    let mut part2_answer: u32 = 0;
 
     let rucksacks = parse_input(input);
-    for r in rucksacks {
-        let err = r.0.intersection(&r.1).next().unwrap();
+    // part one
+    for rucksack in &rucksacks {
+        let r = rucksack.to_string();
+        let n = r.len()/2;
+        let mut c = r.chars();
+        let r1: HashSet<char> = c.by_ref().take(n).collect();
+        let r2: HashSet<char> = c.by_ref().take(n).collect();
+        let err = r1.intersection(&r2).next().unwrap();
         part1_answer += get_priority(*err);
+    }
+
+    // part two
+    let mut ri = rucksacks.iter().peekable();
+    while ri.peek().is_some() {
+        let r0: HashSet<char> = ri.next().unwrap().chars().collect();
+        let r1: HashSet<char> = ri.next().unwrap().chars().collect();
+        let r2: HashSet<char> = ri.next().unwrap().chars().collect();
+        let a: HashSet<char> = r0.intersection(&r1).map(|c| c.clone()).collect();
+        let mut b = a.intersection(&r2);
+        let badge = b.next().unwrap();
+        part2_answer += get_priority(*badge);
     }
 
     (part1_answer, part2_answer)
 }
 
-fn parse_input(input: &'static str) -> Vec<(HashSet<char>, HashSet<char>)> {
-    let mut rucksacks = Vec::new();
-    for rucksack in input.trim_end().split_whitespace() {
-        let r = rucksack.to_string();
-        let n = r.len()/2;
-        let mut c = r.chars();
-        let r1 = c.by_ref().take(n).collect();
-        let r2 = c.by_ref().take(n).collect();
-        rucksacks.push((r1, r2));
-    }
-    rucksacks
+fn parse_input(input: &'static str) -> Vec<&str> {
+    input.trim_end().split_whitespace().collect()
 }
 
 fn get_priority(c: char) -> u32 {
@@ -64,13 +73,13 @@ mod tests {
     fn test_example_answer() {
         let (part1_answer, part2_answer) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 157);
-        assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 70);
     }
 
     #[test]
     fn test_input_answer() {
         let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 7997);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 2545);
     }
 }
