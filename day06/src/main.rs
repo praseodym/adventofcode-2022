36b9e68a272258e09ec1ae30dd0b2a7e5b6d919c
Mark Janssen <mark@praseodym.net>
2022-12-06 06:17:02 +0100

    Day 6: part two

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,24 +3,29 @@ use std::collections::HashSet;
 fn main() {
     let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
-    // println!("part 2 answer: {}", part2_answer);
+    println!("part 2 answer: {}", part2_answer);
 }
 
 fn run(input: &'static str) -> (u32, u32) {
     let input = input.trim();
+    let part1_answer = distinct_chars_offset(input, 4);
+    let part2_answer = distinct_chars_offset(input, 14);
+    (part1_answer, part2_answer)
+}
 
-    for i in 4..input.len() {
+fn distinct_chars_offset(input: &str, n: usize) -> u32 {
+    for i in n..input.len() {
         let mut set = HashSet::new();
-        let mut chars = input.chars().skip(i - 4);
-        for _ in 0..4 {
+        let mut chars = input.chars().skip(i - n);
+        for _ in 0..n {
             let c = chars.next().unwrap();
             set.insert(c);
         }
-        if set.len() == 4 {
-            return (i as u32, 0);
+        if set.len() == n {
+            return i as u32;
         }
     }
-    (0, 0)
+    panic!("offset not found")
 }
 
 #[cfg(test)]
@@ -28,16 +33,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example_answer() {
-        let (part1_answer, part2_answer) = run(include_str!("../input-example"));
-        assert_eq!(part1_answer, 7);
-        assert_eq!(part2_answer, 0);
+    fn test_examples_answer() {
+        assert_eq!((7, 19), run("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assert_eq!((5, 23), run("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!((6, 23), run("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!((10, 29), run("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!((11, 26), run("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
     }
 
     #[test]
     fn test_input_answer() {
         let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 1093);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 3534);
     }
 }
