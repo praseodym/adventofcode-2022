1375833e8714b8aca2b03e6c254f3ee7d72710d0
Mark Janssen <mark@praseodym.net>
2022-12-09 08:54:54 +0100

    Day 9: cleanup

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,129 +1,82 @@
+use std::cmp::Ordering;
+use std::slice::Iter;
+
 fn main() {
     let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
     println!("part 2 answer: {}", part2_answer);
 }
 
-fn run(input: &'static str) -> (u32, u32) {
-    let mut grid = Grid::new();
-
+fn run(input: &'static str) -> (usize, usize) {
     let motions = parse_input(input);
-    for motion in motions {
-        grid.simulate_step(motion.0, motion.1);
-    }
 
-    let part1_answer: u32 = 0;
-    let part2_answer = grid.count_visited();
+    let part1_answer = Rope::simulate(motions.iter(), 2);
+    let part2_answer = Rope::simulate(motions.iter(), 10);
 
     (part1_answer, part2_answer)
 }
 
-const N: isize = 1000;
+const N: usize = 700;
 #[derive(Debug)]
-struct Grid {
-    visited: Box<[[bool; N as usize]; N as usize]>,
-    kx: [isize; 10],
-    ky: [isize; 10],
+struct Rope {
+    visited: Box<[[bool; N]; N]>,
+    kx: Vec<usize>,
+    ky: Vec<usize>,
 }
 
-impl Grid {
-    fn new() -> Self {
-        let mut grid = Grid {
+impl Rope {
+    pub fn simulate(motions: Iter<(char, u8)>, knots: usize) -> usize {
+        let mut rope = Rope::new(knots);
+        for motion in motions {
+            rope.simulate_step(motion.0, motion.1);
+        }
+        rope.count_visited()
+    }
+
+    fn new(knots: usize) -> Self {
+        Rope {
             visited: Box::new([[false; N as usize]; N as usize]),
-            kx: [N / 2; 10],
-            ky: [N / 2; 10],
-        };
-        grid.update_visited();
-        grid
-    }
-
-    fn simulate_step(&mut self, direction: &'static str, steps: u8) {
-        // println!("== {} {} ==\n", direction, steps);
-        match direction {
-            "R" => {
-                for _ in 0..steps {
-                    self.kx[0] += 1;
-                    self.update_knots();
-                }
-            }
-            "L" => {
-                for _ in 0..steps {
-                    self.kx[0] -= 1;
-                    self.update_knots();
-                }
-            }
-            "U" => {
-                for _ in 0..steps {
-                    self.ky[0] -= 1;
-                    self.update_knots();
-                }
-            }
-            "D" => {
-                for _ in 0..steps {
-                    self.ky[0] += 1;
-                    self.update_knots();
-                }
-            }
-            _ => {
-                panic!("unknown: {}", direction)
-            }
+            kx: vec![N / 2; knots],
+            ky: vec![N / 2; knots],
         }
     }
 
-    fn count_visited(&self) -> u32 {
-        let mut count = 0;
-        for y in 0..N as usize {
-            for x in 0..N as usize {
-                if self.visited[y][x] {
-                    // print!("#");
-                    count += 1;
-                } else {
-                    // print!(".");
-                }
-            }
-            // println!();
+    fn simulate_step(&mut self, direction: char, steps: u8) {
+        for _ in 0..steps {
+            match direction {
+                'L' => self.kx[0] -= 1,
+                'R' => self.kx[0] += 1,
+                'U' => self.ky[0] -= 1,
+                'D' => self.ky[0] += 1,
+                _ => panic!("unknown: {}", direction),
+            };
+            self.update_knots();
         }
-        count
     }
 
     fn update_knots(&mut self) {
-        // if self.first {
-        //     self.first = false;
-        //     return;
-        // }
-        // match self.tx.cmp(&self.tx) {
-        //     Ordering::Greater => a(),
-        //     Ordering::Less => b(),
-        //     Ordering::Equal => c()
-        // }
-        for i in 1..10 {
-            let touching = (self.kx[i - 1] == self.kx[i]
-                || self.kx[i - 1] - 1 == self.kx[i]
-                || self.kx[i - 1] + 1 == self.kx[i])
-                && (self.ky[i - 1] == self.ky[i]
-                    || self.ky[i - 1] - 1 == self.ky[i]
-                    || self.ky[i - 1] + 1 == self.ky[i]);
+        for i in 1..self.kx.len() {
+            let touching = (self.ky[i - 1] + 1 == self.ky[i]
+                || self.ky[i - 1] - 1 == self.ky[i]
+                || self.ky[i - 1] == self.ky[i])
+                && (self.kx[i - 1] + 1 == self.kx[i]
+                    || self.kx[i - 1] - 1 == self.kx[i]
+                    || self.kx[i - 1] == self.kx[i]);
 
             if touching {
                 continue;
             }
 
-            if self.kx[i - 1] > self.kx[i] {
-                self.kx[i] += 1;
-            } else if self.kx[i - 1] < self.kx[i] {
-                self.kx[i] -= 1;
-            }
-            if self.ky[i - 1] > self.ky[i] {
-                self.ky[i] += 1;
-            } else if self.ky[i - 1] < self.ky[i] {
-                self.ky[i] -= 1;
+            match self.kx[i - 1].cmp(&self.kx[i]) {
+                Ordering::Greater => self.kx[i] += 1,
+                Ordering::Less => self.kx[i] -= 1,
+                Ordering::Equal => (),
             }
 
-            if self.kx[i] < 0 {
-                self.kx[i] = 0;
-            }
-            if self.ky[i] < 0 {
-                self.ky[i] = 0;
+            match self.ky[i - 1].cmp(&self.ky[i]) {
+                Ordering::Greater => self.ky[i] += 1,
+                Ordering::Less => self.ky[i] -= 1,
+                Ordering::Equal => (),
             }
         }
 
@@ -131,16 +84,26 @@ impl Grid {
     }
 
     fn update_visited(&mut self) {
-        // println!("tail: {} {}", self.tx, self.ty);
-        self.visited[self.ky[9] as usize][self.kx[9] as usize] = true;
+        self.visited[self.ky[self.ky.len() - 1]][self.kx[self.kx.len() - 1]] = true;
+    }
+
+    fn count_visited(&self) -> usize {
+        self.visited
+            .iter()
+            .flat_map(|r| r.iter())
+            .filter(|&&v| v)
+            .count()
     }
 }
 
-fn parse_input(input: &'static str) -> Vec<(&'static str, u8)> {
+fn parse_input(input: &'static str) -> Vec<(char, u8)> {
     let mut ret = Vec::new();
     for line in input.trim_end().split('\n') {
         let mut s = line.split_whitespace();
-        ret.push((s.next().unwrap(), s.next().unwrap().parse::<u8>().unwrap()))
+        ret.push((
+            s.next().unwrap().parse::<char>().unwrap(),
+            s.next().unwrap().parse::<u8>().unwrap(),
+        ))
     }
     ret
 }
@@ -150,15 +113,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example_parse() {
-        let lines = parse_input(include_str!("../input-example"));
+    fn test_example1_parse() {
+        let lines = parse_input(include_str!("../input-example1"));
+        assert_eq!(lines.len(), 8);
+    }
+
+    #[test]
+    fn test_example2_parse() {
+        let lines = parse_input(include_str!("../input-example2"));
         assert_eq!(lines.len(), 8);
     }
 
     #[test]
     fn test_example1_answer() {
-        let (part1_answer, _) = run(include_str!("../input-example"));
-        //assert_eq!(part1_answer, 13);
+        let (part1_answer, _) = run(include_str!("../input-example1"));
+        assert_eq!(part1_answer, 13);
     }
 
     #[test]
@@ -170,7 +139,7 @@ mod tests {
     #[test]
     fn test_input_answer() {
         let (part1_answer, part2_answer) = run(include_str!("../input"));
-        // assert_eq!(part1_answer, 6494);
+        assert_eq!(part1_answer, 6494);
         assert_eq!(part2_answer, 2691);
     }
 }
