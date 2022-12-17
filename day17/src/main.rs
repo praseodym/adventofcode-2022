dfba13c3004ae4500f366fe7786f3e0cb12f3f61
Mark Janssen <mark@praseodym.net>
2022-12-18 00:17:18 +0100

    Day 17: part two

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("part 2 answer: {}", part2_answer);
 }
 
-fn run(input: &'static str) -> (usize, u32) {
+fn run(input: &'static str) -> (usize, usize) {
     let jets = parse_input(input);
     // rocks are upside down
     let rocks = vec![
@@ -25,29 +25,53 @@ fn run(input: &'static str) -> (usize, u32) {
         vec![vec![true, true], vec![true, true]],
     ];
 
+    let part1_answer: usize = simulate(&jets, &rocks, 2022);
+    let part2_answer: usize = simulate(&jets, &rocks, 1000000000000);
+
+    (part1_answer, part2_answer)
+}
+
+fn simulate(jets: &Vec<Jet>, rocks: &Vec<Vec<Vec<bool>>>, num_rocks: usize) -> usize {
     let mut chamber: Chamber = Vec::new();
+    let mut rock_idx: usize = 0;
     let mut jet_idx: usize = 0;
 
-    'spawn_rocks: for rock_idx in 0..2022 {
+    let mut num_rocks = num_rocks;
+    let mut pattern: Option<Pattern> = None;
+    let mut shortcut = 0;
+
+    'spawn_rocks: while rock_idx < num_rocks {
         // SPAWN ROCK
-        println!("== ROCK {} ==", rock_idx + 1);
         let rock = &rocks[rock_idx % rocks.len()];
+        rock_idx += 1;
         let mut movement = Movement::Jet;
         // Each rock appears so that its left edge is two units away from the left wall
         let mut x: isize = 2;
         // and its bottom edge is three units above the highest rock in the room (or the floor, if there isn't one).
         let mut y: isize = (chamber.len() + 3) as isize;
 
+        if shortcut == 0 {
+            if let Some(new_pattern) = check_pattern(&chamber, rock_idx) {
+                if pattern.is_none() {
+                    pattern = Some(new_pattern);
+                } else if let Some(old_pattern) = &pattern {
+                    if new_pattern.cycles > old_pattern.cycles {
+                        if new_pattern.cycles > 3 {
+                            let diff = new_pattern.rocks - old_pattern.rocks;
+                            let remaining_rocks = num_rocks - rock_idx;
+                            num_rocks = rock_idx + (remaining_rocks % diff);
+                            shortcut = (remaining_rocks / diff) * new_pattern.distance;
+                        }
+                        pattern = Some(new_pattern);
+                    }
+                }
+            }
+        }
+
         loop {
-            println!("rock position: x={}, y={}", x, y);
-            println!("  movement: {:?}", movement);
-
-            // print_chamber(&chamber, rock, x, y);
-
             match &movement {
                 Movement::Jet => {
                     let jet = &jets[jet_idx % jets.len()];
-                    println!("  jet: {:?}", jet);
                     match jet {
                         Jet::Left => {
                             if is_clear(&chamber, rock, x - 1, y) {
@@ -75,48 +99,64 @@ fn run(input: &'static str) -> (usize, u32) {
             }
         }
     }
+    shortcut + chamber.len()
+}
 
-    let part1_answer: usize = chamber.len();
-    let part2_answer: u32 = 0;
+#[derive(Debug)]
+struct Pattern {
+    distance: usize,
+    cycles: usize,
+    rocks: usize,
+}
 
-    (part1_answer, part2_answer)
+fn check_pattern(chamber: &Chamber, rocks: usize) -> Option<Pattern> {
+    let h = chamber.len() / 3;
+    for offset in 0..h {
+        'distance: for distance in 10..h {
+            let mut cycles = 0;
+            loop {
+                cycles += 1;
+                if (offset + distance * (cycles + 1)) >= chamber.len() {
+                    break;
+                }
+                for i in 0..distance {
+                    if chamber[offset + i] != chamber[offset + i + distance * cycles] {
+                        continue 'distance;
+                    }
+                }
+            }
+            return Some(Pattern {
+                distance,
+                cycles,
+                rocks,
+            });
+        }
+    }
+    None
 }
 
 fn is_clear(chamber: &Chamber, rock: &[Vec<bool>], x: isize, y: isize) -> bool {
     // check horizontal clearance
     if x < 0 || x + rock[0].len() as isize > 7 {
-        // println!("    no horizontal clearance");
         return false;
     }
 
     // check vertical clearance
     let dy = y - chamber.len() as isize;
     let h = rock.len() as isize;
-    // println!("    distance from chamber: dy={}", dy);
     return if dy >= 0 {
-        println!("    don't have to check chamber");
         true
     } else if -dy > chamber.len() as isize {
-        println!("    chamber too shallow, rock hits floor");
         false
     } else {
         let mdy = cmp::min(dy + h, 0);
-        println!(
-            "    checking for collisions with chamber from i in {}..{}",
-            dy, mdy
-        );
         for i in dy..mdy {
             let ry = (i - dy) as usize;
-            println!(
-                "      checking i={} with mdy={}, dy={}, h={} => ry {}",
-                i, mdy, dy, h, ry
-            );
             let rock_line = &rock[ry];
             for (j, v) in rock_line.iter().enumerate() {
                 let a = (chamber.len() as isize + i) as usize;
                 let b = x as usize + j;
                 if *v && chamber[a][b] {
-                    // println!("    collision!");
                     return false;
                 }
             }
@@ -126,9 +166,7 @@ fn is_clear(chamber: &Chamber, rock: &[Vec<bool>], x: isize, y: isize) -> bool {
 }
 
 fn add_rock(chamber: &mut Chamber, rock: &[Vec<bool>], x: isize, y: isize) {
-    println!("rock comes to rest in chamber");
     let dy = rock.len() as isize + y - chamber.len() as isize;
-    println!("  adding dy={} rows", dy);
     for _ in 0..dy {
         chamber.push([false; 7]);
     }
@@ -139,6 +177,7 @@ fn add_rock(chamber: &mut Chamber, rock: &[Vec<bool>], x: isize, y: isize) {
     }
 }
 
+#[allow(dead_code)]
 fn print_chamber(chamber: &Chamber, rock: &[Vec<bool>], x: isize, y: isize) {
     for ry in (0..y + rock.len() as isize).rev() {
         print!("|");
@@ -202,13 +241,13 @@ mod tests {
     fn test_example_answer() {
         let (part1_answer, part2_answer) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 3068);
-        assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 1514285714288);
     }
 
     #[test]
     fn test_input_answer() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 3193);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 1577650429835);
     }
 }
