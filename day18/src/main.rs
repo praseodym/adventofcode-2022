2d2aa0a12830fa465fb263bd9550f8e7c8b67688
Mark Janssen <mark@praseodym.net>
2022-12-25 17:20:31 +0100

    Day 18: part two WIP

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -5,32 +5,28 @@ fn main() {
 }
 
 fn run(input: &'static str) -> (u32, u32) {
-    let mut part1_answer: u32 = 0;
-    let part2_answer: u32 = 0;
-
     let scan = parse_input(input);
+    let mut part1_answer: u32 = 0;
+
     for x in 0..N {
         for y in 0..N {
             for z in 0..N {
                 if scan[x][y][z] {
-                    if z == 0 || !scan[x][y][z.saturating_sub(1)] {
-                        part1_answer += 1;
-                    }
-                    if !scan[x][y][z + 1] {
-                        part1_answer += 1;
-                    }
-                    if y == 0 || !scan[x][y.saturating_sub(1)][z] {
-                        part1_answer += 1;
-                    }
-                    if !scan[x][y + 1][z] {
-                        part1_answer += 1;
-                    }
-                    if x == 0 || !scan[x.saturating_sub(1)][y][z] {
-                        part1_answer += 1;
-                    }
-                    if !scan[x + 1][y][z] {
-                        part1_answer += 1;
-                    }
+                    println!("{},{},{}", x, y, z);
+                    part1_answer += exposed_surface(scan, x, y, z);
+                }
+            }
+        }
+    }
+
+    let mut part2_answer: u32 = 0;
+    //
+    for x in 0..N {
+        for y in 0..N {
+            for z in 0..N {
+                if scan[x][y][z] {
+                    println!("{},{},{}", x, y, z);
+                    part2_answer += exposed_surface(scan, x, y, z);
                 }
             }
         }
@@ -39,15 +35,38 @@ fn run(input: &'static str) -> (u32, u32) {
     (part1_answer, part2_answer)
 }
 
-const N: usize = 25;
+fn exposed_surface(scan: Scan, x: usize, y: usize, z: usize) -> u32 {
+    let mut count: u32 = 0;
+    if z == 0 || !scan[x][y][z - 1] {
+        count += 1;
+    }
+    if !scan[x][y][z + 1] {
+        count += 1;
+    }
+    if y == 0 || !scan[x][y - 1][z] {
+        count += 1;
+    }
+    if !scan[x][y + 1][z] {
+        count += 1;
+    }
+    if x == 0 || !scan[x - 1][y][z] {
+        count += 1;
+    }
+    if !scan[x + 1][y][z] {
+        count += 1;
+    }
+    count
+}
 
-fn parse_input(input: &'static str) -> [[[bool; N]; N]; N] {
+const N: usize = 30;
+type Scan = [[[bool; N]; N]; N];
+fn parse_input(input: &'static str) -> Scan {
     let mut scan = [[[false; N]; N]; N];
     for line in input.trim_end().split('\n') {
         let mut s = line.split(',');
-        let x = s.next().unwrap().parse::<usize>().unwrap() + 1;
-        let y = s.next().unwrap().parse::<usize>().unwrap() + 1;
-        let z = s.next().unwrap().parse::<usize>().unwrap() + 1;
+        let x = s.next().unwrap().parse::<usize>().unwrap()+5;
+        let y = s.next().unwrap().parse::<usize>().unwrap()+5;
+        let z = s.next().unwrap().parse::<usize>().unwrap()+5;
         scan[x][y][z] = true;
     }
     scan
@@ -69,16 +88,16 @@ mod tests {
 
     #[test]
     fn test_example1_answer() {
-        let (part1_answer, part2_answer) = run(include_str!("../input-example1"));
+        let (part1_answer, _part2_answer) = run(include_str!("../input-example1"));
         assert_eq!(part1_answer, 10);
-        assert_eq!(part2_answer, 0);
     }
 
     #[test]
     fn test_example2_answer() {
         let (part1_answer, part2_answer) = run(include_str!("../input-example2"));
+
         assert_eq!(part1_answer, 64);
-        assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 58);
     }
 
     #[test]
