536ed0e2f827fbb22a7c7700e6447ddb0f39dfee
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-12 15:51:33 +0100

    Day 18: part two

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -15,11 +15,34 @@ const ADJACENCY: [(isize, isize, isize); 6] = [
 ];
 type Scan = [[[bool; N]; N]; N];
 
-fn run(input: &'static str) -> (u32, u32) {
-    let mut part1_answer: u32 = 0;
-    let part2_answer: u32 = 0;
-
+fn run(input: &'static str) -> (usize, usize) {
     let scan = parse_input(input);
+    let part1_answer = calculate_surface_area(scan, None);
+
+    // flood fill to find all voxels that are connected to the outside
+    let mut outside = Scan::default();
+    let mut queue: Vec<(usize, usize, usize)> = vec![(0, 0, 0)];
+    outside[0][0][0] = true;
+    while let Some((x, y, z)) = queue.pop() {
+        for (dx, dy, dz) in ADJACENCY.iter() {
+            let nx = x.saturating_add_signed(*dx);
+            let ny = y.saturating_add_signed(*dy);
+            let nz = z.saturating_add_signed(*dz);
+            if nx >= N || ny >= N || nz >= N || outside[nz][ny][nx] || scan[nz][ny][nx] {
+                continue;
+            }
+            outside[nz][ny][nx] = true;
+            queue.push((nx, ny, nz));
+        }
+    }
+
+    let part2_answer = calculate_surface_area(scan, Some(outside));
+
+    (part1_answer, part2_answer)
+}
+
+fn calculate_surface_area(scan: Scan, outside: Option<[[[bool; 25]; 25]; 25]>) -> usize {
+    let mut surface_area = 0;
     for z in 0..N {
         for y in 0..N {
             for x in 0..N {
@@ -29,15 +52,20 @@ fn run(input: &'static str) -> (u32, u32) {
                         let ny = y.saturating_add_signed(*dy);
                         let nz = z.saturating_add_signed(*dz);
                         if nx >= N || ny >= N || nz >= N || !scan[nz][ny][nx] {
-                            part1_answer += 1;
+                            if let Some(outside) = outside {
+                                if outside[nz][ny][nx] {
+                                    surface_area += 1;
+                                }
+                            } else {
+                                surface_area += 1;
+                            }
                         }
                     }
                 }
             }
         }
     }
-
-    (part1_answer, part2_answer)
+    surface_area
 }
 
 fn parse_input(input: &'static str) -> Scan {
@@ -68,22 +96,21 @@ mod tests {
 
     #[test]
     fn test_example1_answer() {
-        let (part1_answer, part2_answer) = run(include_str!("../input-example1"));
+        let (part1_answer, _) = run(include_str!("../input-example1"));
         assert_eq!(part1_answer, 10);
-        assert_eq!(part2_answer, 0);
     }
 
     #[test]
     fn test_example2_answer() {
         let (part1_answer, part2_answer) = run(include_str!("../input-example2"));
         assert_eq!(part1_answer, 64);
-        assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 58);
     }
 
     #[test]
     fn test_input_answer() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 4512);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 2554);
     }
 }
