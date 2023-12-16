8790c34d74bc48b47edf65bbf6228c2a9b934a92
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-16 16:45:00 +0100

    Day 16: part two

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,18 +1,20 @@
 use regex::Regex;
 use std::cmp;
-use std::collections::HashMap;
+use std::collections::{BTreeSet, HashMap};
 
 fn main() {
-    let (part1_answer, _part2_answer) = run(include_str!("../input"));
+    let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
-    // println!("part 2 answer: {}", part2_answer);
+    println!("part 2 answer: {}", part2_answer);
 }
 
 fn run(input: &'static str) -> (usize, usize) {
     let valves = parse_input(input);
     let shortest_paths = shortest_paths(&valves);
     let part1_answer = max_released_pressure(&valves, &shortest_paths);
-    (part1_answer, 0)
+    // let part1_answer = 0;
+    let part2_answer = max_released_pressure2(&valves, &shortest_paths);
+    (part1_answer, part2_answer)
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -94,13 +96,41 @@ fn shortest_paths(valves: &Vec<Valve>) -> Vec<Vec<usize>> {
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 struct State {
-    opened: Vec<usize>,
+    opened: BTreeSet<usize>,
     pos: usize,
     elapsed: usize,
     relieved: usize,
 }
 
 fn max_released_pressure(valves: &[Valve], distances: &[Vec<usize>]) -> usize {
+    simulate(valves, distances, 30)
+        .values()
+        .copied()
+        .max()
+        .unwrap()
+}
+
+fn max_released_pressure2(valves: &[Valve], distances: &[Vec<usize>]) -> usize {
+    let max_relieved_valves = simulate(valves, distances, 26);
+    let mut max = 0;
+    let max_relieved_valves_vec: Vec<_> = max_relieved_valves.iter().collect();
+    for i in 0..max_relieved_valves_vec.len() {
+        for j in i + 1..max_relieved_valves_vec.len() {
+            let (v1, m1) = max_relieved_valves_vec[i];
+            let (v2, m2) = max_relieved_valves_vec[j];
+            if v1.is_disjoint(v2) {
+                max = cmp::max(max, m1 + m2);
+            }
+        }
+    }
+    max
+}
+
+fn simulate(
+    valves: &[Valve],
+    distances: &[Vec<usize>],
+    time_limit: usize,
+) -> HashMap<BTreeSet<usize>, usize> {
     let usable_valves: Vec<usize> = valves
         .iter()
         .enumerate()
@@ -110,7 +140,7 @@ fn max_released_pressure(valves: &[Valve], distances: &[Vec<usize>]) -> usize {
 
     let start = valves.iter().position(|v| v.name == "AA").unwrap();
     let state = State {
-        opened: Vec::new(),
+        opened: BTreeSet::new(),
         pos: start,
         elapsed: 0,
         relieved: 0,
@@ -118,7 +148,8 @@ fn max_released_pressure(valves: &[Valve], distances: &[Vec<usize>]) -> usize {
     let mut queue: Vec<State> = Vec::new();
     queue.push(state);
 
-    let mut max_released_pressure = 0;
+    let mut max_relieved_valves: HashMap<BTreeSet<usize>, usize> = HashMap::new();
+
     while let Some(state) = queue.pop() {
         let possible = usable_valves
             .iter()
@@ -130,24 +161,27 @@ fn max_released_pressure(valves: &[Valve], distances: &[Vec<usize>]) -> usize {
                 continue;
             }
             let mut opened = state.opened.clone();
-            opened.push(next);
+            opened.insert(next);
             let elapsed = state.elapsed + distance + 1;
-            if elapsed > 30 {
+            if elapsed > time_limit {
                 continue;
             }
-            let relieved = state.relieved + (30 - elapsed) * valves[next].flow_rate;
+            let relieved = state.relieved + (time_limit - elapsed) * valves[next].flow_rate;
             let state = State {
                 opened,
                 pos: next,
                 elapsed,
                 relieved,
             };
-            max_released_pressure = cmp::max(max_released_pressure, state.relieved);
+            max_relieved_valves
+                .entry(state.opened.clone())
+                .and_modify(|val| *val = relieved.max(*val))
+                .or_insert(relieved);
             queue.push(state);
         }
     }
 
-    max_released_pressure
+    max_relieved_valves
 }
 
 #[cfg(test)]
@@ -162,15 +196,15 @@ mod tests {
 
     #[test]
     fn test_example_answer() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example"));
+        let (part1_answer, part2_answer) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 1651);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 1707);
     }
 
     #[test]
     fn test_input_answer() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 1792);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 2587);
     }
 }
